d859194b5e6b0014651da2954221a6664eb1b7e5
Dylan Ballback <Dylanballback19@gmail.com>
2023-10-15 17:10:14 -0400

    uypdate

--- async_test.py
+++ async_test.py
@@ -33,6 +33,45 @@ for msg in bus:
 async def set_vel(vel_set):
     bus.send(can.Message(
         arbitration_id=(node_id << 5 | 0x0d),  # 0x0d: Set_Input_Vel
+        data=struct.pack('<ff', float(vel_set), 0.0),  # 1.0: velocity, 0.0: torque feedforward
+        is_extended_id=False
+    ))
+
+# Print encoder feedback
+async def get_pos_vel():
+    for msg in bus:
+        if msg.arbitration_id == (node_id << 5 | 0x09):  # 0x09: Get_Encoder_Estimates
+            pos, vel = struct.unpack('<ff', bytes(msg.data))
+            print(f"pos: {pos:.3f} [turns], vel: {vel:.3f} [turns/s]")
+            return  # To exit the loop
+
+async def print_values_and_set_vel():
+    value = 0
+    step = 0.25
+
+    while value <= 10:
+        print(value)
+        await set_vel(value)  # Set velocity with the current value
+        value += step
+        await asyncio.sleep(1)
+
+async def main():
+    await asyncio.gather(
+        print_values_and_set_vel(),
+        get_pos_vel()
+    )
+
+if __name__ == "__main__":
+    asyncio.run(main())
+
+
+
+
+"""
+# Set velocity function to vel_set turns/s
+async def set_vel(vel_set):
+    bus.send(can.Message(
+        arbitration_id=(node_id << 5 | 0x0d),  # 0x0d: Set_Input_Vel
         data=struct.pack('<ff', float(vel_set), 0.0),  # velocity, torque feedforward
         is_extended_id=False
     ))
@@ -64,3 +103,7 @@ async def main():
 
 if __name__ == "__main__":
     asyncio.run(main())
+
+
+
+"""
