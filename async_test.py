5bd9a86582f9c7c3648b8621ed07faec61432b1b
Dylan Ballback <Dylanballback19@gmail.com>
2023-10-15 17:05:55 -0400

    update async code

--- async_test.py
+++ async_test.py
@@ -28,11 +28,12 @@ for msg in bus:
 #Don't change the code above
 #-----------------------------------------------------------------------------
 
+
 # Set velocity function to vel_set turns/s
 async def set_vel(vel_set):
     bus.send(can.Message(
         arbitration_id=(node_id << 5 | 0x0d),  # 0x0d: Set_Input_Vel
-        data=struct.pack('<ff', float(vel_set), 0.0),  # 1.0: velocity, 0.0: torque feedforward
+        data=struct.pack('<ff', float(vel_set), 0.0),  # velocity, torque feedforward
         is_extended_id=False
     ))
 
@@ -44,20 +45,21 @@ async def get_pos_vel():
                 pos, vel = struct.unpack('<ff', bytes(msg.data))
                 print(f"pos: {pos:.3f} [turns], vel: {vel:.3f} [turns/s]")
 
-async def print_values_and_set_vel():
-    value = 0
-    step = 0.25
-
-    while value <= 10:
+async def print_and_set_velocity():
+    for value in range(0, 11):
         print(value)
-        await set_vel(value)  # Set velocity with the current value
-        value += step
+        await set_vel(value)  # Set velocity from 0 to 10 in 0.25-second increments
+        await asyncio.sleep(0.25)
+
+    for value in range(10, -1, -1):
+        print(value)
+        await set_vel(value)  # Set velocity from 10 to 0 in 0.25-second increments
         await asyncio.sleep(0.25)
 
 async def main():
     await asyncio.gather(
         get_pos_vel(),  # Continuously run get_pos_vel
-        print_values_and_set_vel()  # Print values and set velocity every 0.25 seconds
+        print_and_set_velocity()  # Print and set velocity
     )
 
 if __name__ == "__main__":
