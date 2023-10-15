58651e2859fc259acc83ce6b05e51be9e3cc3f98
Dylan Ballback <Dylanballback19@gmail.com>
2023-10-15 16:11:00 -0400

    update

--- can_reset.sh
+++ can_reset.sh
@@ -1,10 +1,10 @@
 sudo /sbin/ip link set can0 down
 sudo ip link set can0 type can restart-ms 100
-ip link set can0 type can restart
+#ip link set can0 type can restart
 
 #echo("Reset was Succesful")
 
 sudo ip link set can0 up type can bitrate 250000
 candump can0 -xct z -n 10
 
-#cowsay -f elephant "Succesful Conection"
+cowsay -f dragon "Succesful Conection"
