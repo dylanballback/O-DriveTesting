1cfa0df4e6b4982a9e5ffce21624d2b039f23d9a
Dylan Ballback <Dylanballback19@gmail.com>
2023-10-15 17:24:52 -0400

    try again

--- async_test2.py
+++ async_test2.py
@@ -29,47 +29,60 @@ for msg in bus:
 
 #Don't change the code above
 #-----------------------------------------------------------------------------
+import can
+import struct
+import threading
+import time
 
+node_id = 0
+bus = can.interface.Bus("can0", bustype="socketcan")
+
+# Flush CAN RX buffer so there are no more old pending messages
+while not (bus.recv(timeout=0) is None):
+    pass
+
+# Put the axis into closed loop control state
+bus.send(can.Message(
+    arbitration_id=(node_id << 5 | 0x07),
+    data=struct.pack('<I', 8),
+    is_extended_id=False
+))
+
+# Wait for the axis to enter closed loop control by scanning heartbeat messages
+for msg in bus:
+    if msg.arbitration_id == (node_id << 5 | 0x01):
+        error, state, result, traj_done = struct.unpack('<IBBB', bytes(msg.data[:7]))
+        if state == 8:
+            break
 
 # Set velocity function to vel_set turns/s
-def set_vel(vel_set):
-    bus.send(can.Message(
-        arbitration_id=(node_id << 5 | 0x0d),  # 0x0d: Set_Input_Vel
-        data=struct.pack('<ff', float(vel_set), 0.0),  # velocity, torque feedforward
-        is_extended_id=False
-    ))
+def set_vel():
+    for vel_set in range(11):
+        bus.send(can.Message(
+            arbitration_id=(node_id << 5 | 0x0d),
+            data=struct.pack('<ff', float(vel_set), 0.0),
+            is_extended_id=False
+        ))
+        time.sleep(0.25)
 
 # Print encoder feedback
 def get_pos_vel():
-    while True:
-        for msg in bus:
-            if msg.arbitration_id == (node_id << 5 | 0x09):  # 0x09: Get_Encoder_Estimates
-                pos, vel = struct.unpack('<ff', bytes(msg.data))
-                print(f"pos: {pos:.3f} [turns], vel: {vel:.3f} [turns/s]")
+    for msg in bus:
+        if msg.arbitration_id == (node_id << 5 | 0x09):
+            pos, vel = struct.unpack('<ff', bytes(msg.data))
+            print(f"pos: {pos:.3f} [turns], vel: {vel:.3f} [turns/s]")
 
-def main():
-    # Create and start threads for get_pos_vel and set_vel functions
-    get_pos_vel_thread = threading.Thread(target=get_pos_vel)
-    get_pos_vel_thread.daemon = True  # Allow program to exit if only this thread is running
-    get_pos_vel_thread.start()
+# Create two threads to run set_vel and get_pos_vel concurrently
+vel_thread = threading.Thread(target=set_vel)
+pos_thread = threading.Thread(target=get_pos_vel)
 
-    value = 0
-    step = 0.25
+# Start both threads
+vel_thread.start()
+pos_thread.start()
 
-    while value <= 10:
-        print(value)
-        set_vel(value)  # Set velocity from 0 to 10 in 0.25-second increments
-        value += step
-        time.sleep(0.25)
-
-    while value >= 0:
-        print(value)
-        set_vel(value)  # Set velocity from 10 to 0 in 0.25-second increments
-        value -= step
-        time.sleep(0.25)
-
-if __name__ == "__main__":
-    main()
+# Wait for both threads to finish
+vel_thread.join()
+pos_thread.join()
 
 
 
