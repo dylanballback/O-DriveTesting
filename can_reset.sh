e0aad092a8f1960cdcb125f133f5724393de7c81
Dylan Ballback <Dylanballback19@gmail.com>
2023-10-15 14:50:06 -0400

    added can_reset.sh file

--- can_reset.sh
+++ can_reset.sh
@@ -1,3 +1,8 @@
+sudo /sbin/ip link set can0 down
+sudo ip link set can0 type can restart-ms 100
+
+echo("Reset was Succesful")
+
 sudo ip link set can0 up type can bitrate 250000
 candump can0 -xct z -n 10
 
