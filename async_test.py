450f1f16c31d054a1542a0df283033e61a247c94
Dylan Ballback <Dylanballback19@gmail.com>
2023-10-15 16:58:15 -0400

    update async to get pos_vel data every cycle

--- async_test.py
+++ async_test.py
@@ -2,26 +2,9 @@ import asyncio
 import can
 import struct
 
-node_id = 0 # must match `<odrv>.axis0.config.can.node_id`. The default is 0.
-
-bus = can.interface.Bus("can0", bustype="socketcan")
-
-# Flush CAN RX buffer so there are no more old pending messages
-while not (bus.recv(timeout=0) is None): pass
-
-# Put axis into closed loop control state
-bus.send(can.Message(
-    arbitration_id=(node_id << 5 | 0x07), # 0x07: Set_Axis_State
-    data=struct.pack('<I', 8), # 8: AxisState.CLOSED_LOOP_CONTROL
-    is_extended_id=False
-))
-
-# Wait for axis to enter closed loop control by scanning heartbeat messages
-for msg in bus:
-    if msg.arbitration_id == (node_id << 5 | 0x01): # 0x01: Heartbeat
-        error, state, result, traj_done = struct.unpack('<IBBB', bytes(msg.data[:7]))
-        if state == 8: # 8: AxisState.CLOSED_LOOP_CONTROL
-            break
+# Example values for node_id and bus, you should replace them with your actual values
+node_id = 1
+bus = can.interface.Bus(channel='can0', bustype='socketcan')
 
 # Set velocity function to vel_set turns/s
 async def set_vel(vel_set):
@@ -33,12 +16,13 @@ async def set_vel(vel_set):
 
 # Print encoder feedback
 async def get_pos_vel():
-    for msg in bus:
-        if msg.arbitration_id == (node_id << 5 | 0x09):  # 0x09: Get_Encoder_Estimates
-            pos, vel = struct.unpack('<ff', bytes(msg.data))
-            print(f"pos: {pos:.3f} [turns], vel: {vel:.3f} [turns/s]")
-            return  # To exit the loop
+    while True:
+        for msg in bus:
+            if msg.arbitration_id == (node_id << 5 | 0x09):  # 0x09: Get_Encoder_Estimates
+                pos, vel = struct.unpack('<ff', bytes(msg.data))
+                print(f"pos: {pos:.3f} [turns], vel: {vel:.3f} [turns/s]")
 
+# This task runs the print_values and set_vel concurrently
 async def print_values_and_set_vel():
     value = 0
     step = 0.25
@@ -50,10 +34,14 @@ async def print_values_and_set_vel():
         await asyncio.sleep(1)
 
 async def main():
-    await asyncio.gather(
-        print_values_and_set_vel(),
-        get_pos_vel()
-    )
+    # Create a task for get_pos_vel to run continuously in the background
+    background_task = asyncio.create_task(get_pos_vel())
+    
+    # Run print_values_and_set_vel
+    await print_values_and_set_vel()
+    
+    # Cancel the background_task when print_values_and_set_vel completes
+    background_task.cancel()
 
 if __name__ == "__main__":
     asyncio.run(main())
