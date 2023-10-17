c2e60413d3d3198ffc6f9dd48d5a7512d3841319
Dylan Ballback <Dylanballback19@gmail.com>
2023-10-16 23:05:45 -0400

    udpate

--- websocket/basic.py
+++ websocket/basic.py
@@ -50,50 +50,46 @@ def connect_to_websocket(server_ip, server_port):
         print(f"Failed to connect to the WebSocket server. Error: {e}")
         return None
 
-def get_imu_angles(sensor, calibration_data, websocket):
-    """Read IMU angles and send data via WebSocket."""
+
+def get_imu_angles(sensor, calibration_data):
+    """Read IMU angles and return them."""
     alpha = 0.98
     angle_pitch = angle_roll = 0.0
     previous_time = time.monotonic()
 
-    while True:
-        current_time = time.monotonic()
-        elapsed_time = current_time - previous_time
-        accel_x, accel_y, accel_z = sensor.acceleration
-        gyro_x, gyro_y, gyro_z = sensor.gyro
+    current_time = time.monotonic()
+    elapsed_time = current_time - previous_time
+    accel_x, accel_y, accel_z = sensor.acceleration
+    gyro_x, gyro_y, gyro_z = sensor.gyro
 
-        for i in range(3):
-            gyro_x -= calibration_data["gyro_total"][i]
-            gyro_y -= calibration_data["gyro_total"][i]
-            gyro_z -= calibration_data["gyro_total"][i]
+    for i in range(3):
+        gyro_x -= calibration_data["gyro_total"][i]
+        gyro_y -= calibration_data["gyro_total"][i]
+        gyro_z -= calibration_data["gyro_total"][i]
 
-        pitch_acc = math.atan2(accel_x, math.sqrt(accel_y * accel_y + accel_z * accel_z)) * (180 / math.pi)
-        roll_acc = math.atan2(accel_y, accel_z) * (180 / math.pi)
+    pitch_acc = math.atan2(accel_x, math.sqrt(accel_y * accel_y + accel_z * accel_z)) * (180 / math.pi)
+    roll_acc = math.atan2(accel_y, accel_z) * (180 / math.pi)
 
-        pitch_gyro = angle_pitch + gyro_x * elapsed_time
-        roll_gyro = angle_roll + gyro_y * elapsed_time
+    pitch_gyro = angle_pitch + gyro_x * elapsed_time
+    roll_gyro = angle_roll + gyro_y * elapsed_time
 
-        angle_pitch = alpha * pitch_gyro + (1 - alpha) * pitch_acc
-        angle_roll = alpha * roll_gyro + (1 - alpha) * roll_acc
+    angle_pitch = alpha * pitch_gyro + (1 - alpha) * pitch_acc
+    angle_roll = alpha * roll_gyro + (1 - alpha) * roll_acc
 
-        print(f"Pitch: {angle_pitch:.2f} degrees, Roll: {angle_roll:.2f} degrees")
-
-        if websocket:
-            try:
-                data = {'pitch': angle_pitch, 'roll': angle_roll}
-                websocket.emit('imu_data', data)
-            except Exception as e:
-                print(f"Failed to send data through WebSocket. Error: {e}")
-
-        previous_time = current_time
-        time.sleep(.1)
+    return angle_pitch, angle_roll
 
 def main():
     """Main execution function."""
     imu_sensor = initialize_imu()
     imu_calibration_data = calibrate_imu(imu_sensor)
     ws = connect_to_websocket('192.168.1.2', 5025)
-    get_imu_angles(imu_sensor, imu_calibration_data, ws)
+    
+    while True:
+        pitch, roll = get_imu_angles(imu_sensor, imu_calibration_data)
+        print(f"Pitch: {pitch:.2f} degrees, Roll: {roll:.2f} degrees")
+        data = {'pitch': pitch, 'roll': roll}
+        send_data_via_websocket(ws, data)
+        time.sleep(.1)
 
 if __name__ == "__main__":
     main()
