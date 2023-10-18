97c0e3343eb429b7f85b510e93e56edb5f93f782
Dylan Ballback <Dylanballback19@gmail.com>
2023-10-18 01:02:29 -0400

    update

--- muiltiple_odrives/pos_multi.py
+++ muiltiple_odrives/pos_multi.py
@@ -2,21 +2,18 @@ import can
 import struct
 import threading
 import time
-import signal
 import sys
 
 # Define the node IDs for your ODrives
 odrive_node_ids = [0, 1, 2]
-bus_lock = threading.Lock()
 
 # Function to set position for a specific ODrive
 def set_position(node_id, position):
-    with bus_lock:
-        bus.send(can.Message(
-            arbitration_id=(node_id << 5 | 0x0a),
-            data=struct.pack('<ff', float(position), 0.0),
-            is_extended_id=False
-        ))
+    bus.send(can.Message(
+        arbitration_id=(node_id << 5 | 0x0a),
+        data=struct.pack('<ff', float(position), 0.0),
+        is_extended_id=False
+    ))
 
 # Define the CAN bus interface
 bus = can.interface.Bus("can0", bustype="socketcan")
@@ -24,18 +21,6 @@ bus = can.interface.Bus("can0", bustype="socketcan")
 # Flush CAN RX buffer
 while not (bus.recv(timeout=0) is None):
     pass
-
-# Function to get position and velocity for a specific ODrive
-def get_pos_vel(node_id):
-    while True:
-        with bus_lock:
-            for msg in bus:
-                if msg.arbitration_id == (node_id << 5 | 0x09):
-                    pos, vel = struct.unpack('<ff', bytes(msg.data))
-                    # Ensure each print statement goes to a new line
-                    print(f"ODrive {node_id} - Position: {pos:.3f} [turns], Velocity: {vel:.3f} [turns/s]")
-                    time.sleep(0.5)
-                    break
 
 # Function to connect to and configure an ODrive
 def connect_odrive(node_id):
@@ -59,17 +44,11 @@ def move_odrive_position(node_id):
         set_position(node_id, position)
         time.sleep(3)
 
-# Start threads
+# Start threads to move position
 for node_id in odrive_node_ids:
-    # Start a thread for each ODrive to move its position
     position_thread = threading.Thread(target=move_odrive_position, args=(node_id,))
     position_thread.daemon = True
     position_thread.start()
-
-    # Start a thread for each ODrive to continuously fetch and print its position and velocity
-    pos_vel_thread = threading.Thread(target=get_pos_vel, args=(node_id,))
-    pos_vel_thread.daemon = True
-    pos_vel_thread.start()
 
 try:
     while True:
@@ -77,7 +56,6 @@ try:
 
 except KeyboardInterrupt:
     print("\nKeyboard interrupt detected. Stopping all ODrives.")
-    # Stop all motors
     for node_id in odrive_node_ids:
         set_position(node_id, 0)
     bus.shutdown()
