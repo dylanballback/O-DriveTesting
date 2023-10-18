7cb6dabceb71a52c84bf2a60c1aafd3f9e53ce37
Dylan Ballback <Dylanballback19@gmail.com>
2023-10-18 00:58:31 -0400

    update

--- muiltiple_odrives/pos_multi.py
+++ muiltiple_odrives/pos_multi.py
@@ -7,19 +7,16 @@ import sys
 
 # Define the node IDs for your ODrives
 odrive_node_ids = [0, 1, 2]
+bus_lock = threading.Lock()
 
 # Function to set position for a specific ODrive
 def set_position(node_id, position):
-    bus.send(can.Message(
-        arbitration_id=(node_id << 5 | 0x0a),
-        data=struct.pack('<ff', float(position), 0.0),
-        is_extended_id=False
-    ))
-
-# Function to stop all ODrives
-def stop_all_odrives():
-    for node_id in odrive_node_ids:
-        set_position(node_id, 0)
+    with bus_lock:
+        bus.send(can.Message(
+            arbitration_id=(node_id << 5 | 0x0a),
+            data=struct.pack('<ff', float(position), 0.0),
+            is_extended_id=False
+        ))
 
 # Define the CAN bus interface
 bus = can.interface.Bus("can0", bustype="socketcan")
@@ -31,12 +28,14 @@ while not (bus.recv(timeout=0) is None):
 # Function to get position and velocity for a specific ODrive
 def get_pos_vel(node_id):
     while True:
-        for msg in bus:
-            if msg.arbitration_id == (node_id << 5 | 0x09):
-                pos, vel = struct.unpack('<ff', bytes(msg.data))
-                print(f"\rODrive {node_id} - Position: {pos:.3f} [turns], Velocity: {vel:.3f} [turns/s]", end="")
-                break
-        time.sleep(0.5)  # Polling delay
+        with bus_lock:
+            for msg in bus:
+                if msg.arbitration_id == (node_id << 5 | 0x09):
+                    pos, vel = struct.unpack('<ff', bytes(msg.data))
+                    # Ensure each print statement goes to a new line
+                    print(f"ODrive {node_id} - Position: {pos:.3f} [turns], Velocity: {vel:.3f} [turns/s]")
+                    time.sleep(0.5)
+                    break
 
 # Function to connect to and configure an ODrive
 def connect_odrive(node_id):
@@ -60,8 +59,9 @@ def move_odrive_position(node_id):
         set_position(node_id, position)
         time.sleep(3)
 
-# Start a thread for each ODrive to move its position
+# Start threads
 for node_id in odrive_node_ids:
+    # Start a thread for each ODrive to move its position
     position_thread = threading.Thread(target=move_odrive_position, args=(node_id,))
     position_thread.daemon = True
     position_thread.start()
@@ -77,6 +77,8 @@ try:
 
 except KeyboardInterrupt:
     print("\nKeyboard interrupt detected. Stopping all ODrives.")
-    stop_all_odrives()
+    # Stop all motors
+    for node_id in odrive_node_ids:
+        set_position(node_id, 0)
     bus.shutdown()
     sys.exit(0)
