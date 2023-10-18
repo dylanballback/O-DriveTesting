560677ddb06cc54288cb907d46037da1a00509ec
Dylan Ballback <Dylanballback19@gmail.com>
2023-10-18 01:11:54 -0400

    update

--- muiltiple_odrives/pos_simple.py
+++ muiltiple_odrives/pos_simple.py
@@ -18,13 +18,21 @@ def clear_can_buffer():
         if msg is None:
             break
 
+def connect_odrive(node_id):
+    print(f"Attempting to connect to ODrive {node_id}...")
+    # Placeholder for actual connection logic if needed
+    time.sleep(0.5)  # Simulate some delay for the connection
+    print(f"Successfully connected to ODrive {node_id}")
+    set_position(node_id, 0)
+
 bus = can.interface.Bus("can0", bustype="socketcan")
 
 # Clear any pending messages on the CAN bus
 clear_can_buffer()
 
+# Connect to each ODrive
 for node_id in odrive_node_ids:
-    set_position(node_id, 0)
+    connect_odrive(node_id)
 
 positions = {node_id: 0 for node_id in odrive_node_ids}
 
