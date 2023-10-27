1723b18c8fbfe7558fbefed41ac0a1bb823c969b
Dylan Ballback <Dylanballback19@gmail.com>
2023-10-27 12:16:58 -0400

    comments

--- basicCAN_Control/motor_config.py
+++ basicCAN_Control/motor_config.py
@@ -23,11 +23,13 @@ class ODriveCAN:
 
 
 
-
-
-    def setNodeID(self):
+    def setAxisNodeID(self):
         """
-        Sets NodeID for an O-Drive Controller through CAN BUS
+        Sets Axis NodeID for an O-Drive Controller through CAN BUS
+
+        Set_Axis_NodeID: 0x06
+
+        
         """
 
         pass
@@ -36,5 +38,79 @@ class ODriveCAN:
 
     def setAxisState(self):
         """
+        Set Axis State for an O-Drive Controller through CAN BUS
+
         
+        CAN Set_Axis_State: 0x07
+            Axis_Requested_State:
+                Undefined:                           0x0
+                Idle:                                0x1
+                Startup_Sequence:                    0x2
+                Full_Calibration_Sequence:           0x3
+                Motor_Calibration:                   0x4 
+                Encoder_Index_Search:                0x5
+                Encoder_Offset_Calibration:          0x6
+                Closed_Loop_Control:                 0x7
+                Lockin_Spin:                         0x8
+                Encoder_DIR_Find:                    0x9
+                Homing:                              0xA
+                Encoder_Hall_Polarity_Calibration:   0xB
+                Encoder_Hall_Phase_Calibration:      0xD
+
+
         """
+        pass
+
+
+
+    def getAxisEncoderEstimates(self):
+        """
+        Get Encoder Estimates for specific O-Drive Controller Axis through CAN BUS
+
+        CAN Get_Encoder_Estimates: 0x09
+                    - Pos_Estimate 
+                    - Vel_Estimate
+
+        Attributes:
+            Axis_ID 
+
+
+        Returns:
+            Pos_Estimate
+            Vel_Estimate 
+            
+
+
+        """
+        pass
+
+
+
+
+    def setControllerMode(self):
+        """
+        Set the O-Drive Controller Mode type 
+
+        Attribute: 
+            CAN Set_Controller_Mode: 0x0B
+                    Control_Mode:
+                        Voltage_Control:   0x0
+                        Torque_Control:    0x1
+                        Velocity_Control:  0x2
+                        Position_Control:  0x3
+
+                    Input_Mode:
+                        Inactive:      0x0
+                        Passthrough:   0x1
+                        VEL_Ramp:      0x2
+                        Pos_Filter:    0x3
+                        Mix_Channels:  0x4
+                        Trap_Traj:     0x5
+                        Torque_Ramp:   0x6
+                        Mirror:        0x7  
+                        Tunning:       0x8
+            
+        """
+        pass
+
+
