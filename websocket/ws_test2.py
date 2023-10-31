64251f6f2b36b76dbe012f5f03fddc7eb6bd1839
Dylan Ballback <Dylanballback19@gmail.com>
2023-10-31 16:44:08 -0400

    update socketio

--- websocket/ws_test2.py
+++ websocket/ws_test2.py
@@ -1,14 +1,25 @@
-# client.py
-import asyncio
-import websockets
-import json
+import socketio
 
-async def send_data():
-    uri = "ws://192.168.1.2:5025/socket.io/?EIO=4&transport=websocket"  # Change server_address to your server's IP
-    async with websockets.connect(uri) as websocket:
-        data = [1, 2, 3, 4, 5]  # Your list of integers
-        # Flask-SocketIO uses a custom message format, so we need to format it properly:
-        message = '42["send_data", {}]'.format(json.dumps(data))
-        await websocket.send(message)
+sio = socketio.Client()
 
-asyncio.get_event_loop().run_until_complete(send_data())
+@sio.event
+def connect():
+    print('Connected to server')
+    send_data()
+
+@sio.event
+def disconnect():
+    print('Disconnected from server')
+
+def send_data():
+    data = [1, 2, 3, 4, 5]
+    sio.emit('send_data', data)
+    print(f"Sent data: {data}")
+
+if __name__ == '__main__':
+    try:
+        # Replace 'server_address' with the IP of the machine running the Flask server
+        sio.connect('http://server_address:5000')
+        sio.wait()
+    except Exception as e:
+        print(f"Error: {e}")
