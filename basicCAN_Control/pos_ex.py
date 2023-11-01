c20452f2970b922b3856cb423f090066434fdfa5
Dylan Ballback <Dylanballback19@gmail.com>
2023-11-01 17:59:14 -0400

    update

--- basicCAN_Control/pos_ex.py
+++ basicCAN_Control/pos_ex.py
@@ -5,8 +5,10 @@ node_id = 0 # must match `<odrv>.axis0.config.can.node_id`. The default is 0.
 
 bus = can.interface.Bus("can0", bustype="socketcan")
 
+
 # Flush CAN RX buffer so there are no more old pending messages
 while not (bus.recv(timeout=0) is None): pass
+
 
 # Put axis into closed loop control state
 bus.send(can.Message(
@@ -15,12 +17,14 @@ bus.send(can.Message(
     is_extended_id=False
 ))
 
+
 # Wait for axis to enter closed loop control by scanning heartbeat messages
 for msg in bus:
     if msg.arbitration_id == (node_id << 5 | 0x01): # 0x01: Heartbeat
         error, state, result, traj_done = struct.unpack('<IBBB', bytes(msg.data[:7]))
         if state == 8: # 8: AxisState.CLOSED_LOOP_CONTROL
             break
+
 
 def set_odrive_position(node_id, desired_position, bus_channel="can0"):
     """
@@ -60,4 +64,4 @@ def get_pos_vel():
 
 
 # Example usage:
-# set_odrive_position(0, 1000.0)  # Node ID 0, desired position 1000.0 units
+set_odrive_position(0, 1000.0)  # Node ID 0, desired position 1000.0 units
--- muiltiple_odrives/simple_pos.py
+++ muiltiple_odrives/simple_pos.py
@@ -9,17 +9,20 @@ odrive_node_ids = [0, 1, 2]
 
 bus = can.interface.Bus("can0", bustype="socketcan")
 
+
 def set_position(node_id, position):
     bus.send(can.Message(
         arbitration_id=(node_id << 5 | 0x0a),
         data=struct.pack('<ff', float(position), 0.0, 0.0),
         is_extended_id=False
     ))
+    print(f"Successfully moved ODrive {node_id} to {position}")
     
 
 def flush_can_buffer():
     """Flush CAN RX buffer to ensure no old pending messages."""
     while not (bus.recv(timeout=0) is None): pass
+
 
 def connect_odrive(node_id):
     print(f"Attempting to connect to ODrive {node_id}...")
@@ -57,13 +60,6 @@ def set_control_state(node_id):
         print(f"Error connecting to ODrive {node_id}: {str(e)}")
 
 
-def move_odrive_position(node_id):
-    position = 0
-    while True:
-        position += 300
-        set_position(node_id, position)
-        print(f"Successfully moved ODrive {node_id} to {position}")
-        time.sleep(3)
 
 
 if __name__ == "__main__":
@@ -73,6 +69,7 @@ if __name__ == "__main__":
         set_control_state(node_id)
 
     position = 0
+
     for node_id in odrive_node_ids:
         position += 300
         set_position(node_id, position)
