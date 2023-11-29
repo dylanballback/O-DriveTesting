2eb18eb500383bf6fe37ca26c58b94df9e80184c
Dylan Ballback <Dylanballback19@gmail.com>
2023-11-28 21:51:42 -0500

    update

--- encoder/basic_test.py
+++ encoder/basic_test.py
@@ -2,24 +2,18 @@ import smbus
 import time
 
 bus = smbus.SMBus(2)  # 1 indicates /dev/i2c-1
-address = 0x3A # AS5048A I2C address
+address = 0x40# AS5048A I2C address
 
-
-def read_angle_raw():
-    msb = bus.read_byte_data(address, 0xFE)
-    lsb = bus.read_byte_data(address, 0xFF)
-    return msb, lsb
-
-def convert_to_angle(msb, lsb):
-    # Combine the two bytes to create a 14-bit angle value
-    # Mask the MSB with 0x3F to get rid of the two most significant non-angle bits
-    angle = ((msb & 0x3F) << 8) | lsb
-    # Convert the raw sensor reading to degrees (optional)
-    angle_in_degrees = (angle / 0x3FFF) * 360.0
-    return angle_in_degrees
+def read_angle():
+    # Read the angle value from the sensor
+    data = bus.read_i2c_block_data(address, 0xFE, 2)
+    # Combine the MSB and LSB into a single 14-bit value
+    angle = (data[0] << 6) | (data[1] & 0x3F)
+    return angle
 
 while True:
-    msb, lsb = read_angle_raw()
-    angle = convert_to_angle(msb, lsb)
-    print(f"Raw MSB: {msb}, Raw LSB: {lsb}, Angle: {angle:.2f} degrees")
+    angle_raw = read_angle()
+    # Convert the raw angle to degrees
+    angle_degrees = (angle_raw / float(0x3FFF)) * 360.0
+    print(f"Raw angle value: {angle_raw}, Angle in degrees: {angle_degrees:.2f}")
     time.sleep(0.1)  # Sleep for 100ms
