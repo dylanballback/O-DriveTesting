95e5bbfab9e3f733be215aa39d11c9a551b69796
Dylan Ballback <Dylanballback19@gmail.com>
2023-11-29 10:16:19 -0500

    7

--- encoder/basic_test.py
+++ encoder/basic_test.py
@@ -2,15 +2,24 @@ import smbus
 import time
 
 bus = smbus.SMBus(2)  # 1 indicates /dev/i2c-1
-address = 0x50# AS5048A I2C address
+address = 0x50 #AS5048A I2C address
+
+
+def read_register(reg):
+    return bus.read_byte_data(address, reg)
 
 def read_angle():
-    data = bus.read_i2c_block_data(address, 0xFF, 2)
-    angle = (data[0] << 8) | data[1]
-    angle = angle & 0x3FFF  # 14-bit angle
-    return angle
+    data = bus.read_i2c_block_data(address, 0xFE, 2)
+    return (data[0] << 6) | (data[1] & 0x3F)
+
+def diagnose_sensor():
+    agc = read_register(0xFA)  # Automatic Gain Control register
+    diag = read_register(0xFB)  # Diagnostics register
+    print(f"AGC: {agc}, DIAG: {diag}")
 
 while True:
-    angle = read_angle()
-    print("Angle: ", angle)
-    time.sleep(1)
+    angle_raw = read_angle()
+    angle_degrees = (angle_raw / float(0x3FFF)) * 360.0
+    print(f"Raw angle value: {angle_raw}, Angle in degrees: {angle_degrees:.2f}")
+    diagnose_sensor()
+    time.sleep(0.1)
