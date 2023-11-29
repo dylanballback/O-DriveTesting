694efec007456ecf77c834d29189d6954d369eda
Dylan Ballback <Dylanballback19@gmail.com>
2023-11-29 10:49:39 -0500

    a

--- encoder/basic_test.py
+++ encoder/basic_test.py
@@ -4,20 +4,24 @@ import time
 # AS5048B I2C address
 AS5048B_ADDRESS = 0x40
 
-# Registers for the AS5048B
-AS5048B_REG_ANGLE = 0xFE  # The angle register is 0xFE
+# Registers for the AS5048B angle value
+AS5048B_REG_ANGLE_HIGH = 0xFE  # High byte of the angle value
+AS5048B_REG_ANGLE_LOW = 0xFF   # Low byte of the angle value
 
 def read_angle(bus, address):
     """
     Read the angle data from the encoder.
     """
-    # Read two bytes of data
-    data = bus.read_i2c_block_data(address, AS5048B_REG_ANGLE, 2)
+    # Read two bytes of data from the angle registers
+    angle_high = bus.read_byte_data(address, AS5048B_REG_ANGLE_HIGH)
+    angle_low = bus.read_byte_data(address, AS5048B_REG_ANGLE_LOW)
 
     # Combine the two bytes to create one 14-bit number
-    angle = ((data[0] & 0xFF) << 6) | (data[1] & 0x3F)
+    # Masking the lower 6 bits of the high byte and shifting left by 8 bits,
+    # then adding the low byte.
+    angle = ((angle_high & 0x3F) << 8) | angle_low
 
-    # The value is 14-bit, so we can normalize it to a 360 degree scale
+    # The value is 14-bit, so we normalize it to a 360 degree scale
     return angle * 360 / 16384.0
 
 def main():
