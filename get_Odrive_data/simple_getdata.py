7fc8fdbdaaa3943601fd9558bbd7d9dc0cba1574
Dylan Ballback <44906769+dylanballback@users.noreply.github.com>
2023-12-01 13:19:07 -0500

    Update simple_getdata.py
    add RTR functions for getting data

--- get_Odrive_data/simple_getdata.py
+++ get_Odrive_data/simple_getdata.py
@@ -81,3 +81,104 @@ def get_iq_setpoint_measured(node_id):
             iq_setpoint, iq_measured = struct.unpack('<ff', bytes(msg.data))
             print(f"O-Drive {node_id} - Iq Setpoint: {iq_setpoint:.3f} [A], Iq Measured: {iq_measured:.3f} [A]")
 
+
+
+
+"""
+#Using RTR to get data (so you don't have to set cyclic message rate)
+
+import can
+import struct
+import time
+
+odrive_node_ids = [0, 1]
+bus = can.interface.Bus("can0", bustype="socketcan")
+
+def send_rtr_message(node_id, request_id):
+    try:
+        # Create an RTR frame
+        rtr_frame = can.Message(
+            arbitration_id=(node_id << 5 | request_id),
+            is_remote_frame=True,
+            is_extended_id=False
+        )
+
+        # Send the RTR frame
+        bus.send(rtr_frame)
+
+        # Wait for a response
+        response = bus.recv(timeout=1.0)
+
+        if response:
+            handle_response(node_id, request_id, response)
+        else:
+            print(f"No response received for ODrive {node_id}, request_id {request_id}")
+
+    except Exception as e:
+        print(f"Error sending RTR message to ODrive {node_id}, request_id {request_id}: {str(e)}")
+
+
+def handle_response(node_id, request_id, response):
+    if request_id == 0x07:  # Set_Axis_State
+        handle_set_control_state_response(node_id, response)
+    elif request_id == 0x09:  # Get_Encoder_Estimates
+        handle_encoder_estimate_response(node_id, response)
+    elif request_id == 0x1C:  # Get_Torques
+        handle_torque_response(node_id, response)
+    elif request_id == 0x17:  # Get_Bus_Voltage_Current
+        handle_bus_voltage_current_response(node_id, response)
+    elif request_id == 0x14:  # Get_Iq
+        handle_iq_setpoint_measured_response(node_id, response)
+    else:
+        print(f"Unhandled response for ODrive {node_id}, request_id {request_id}")
+
+
+def handle_set_control_state_response(node_id, response):
+    # Add logic to handle the response for Set_Axis_State if needed
+    pass
+
+
+def handle_encoder_estimate_response(node_id, response):
+    pos, vel = struct.unpack('<ff', bytes(response.data))
+    print(f"O-Drive {node_id} - pos: {pos:.3f} [turns], vel: {vel:.3f} [turns/s]")
+
+
+def handle_torque_response(node_id, response):
+    torque_target, torque_estimate = struct.unpack('<ff', bytes(response.data))
+    print(f"O-Drive {node_id} - Torque Target: {torque_target:.3f} [Nm], Torque Estimate: {torque_estimate:.3f} [Nm]")
+
+
+def handle_bus_voltage_current_response(node_id, response):
+    bus_voltage, bus_current = struct.unpack('<ff', bytes(response.data))
+    print(f"O-Drive {node_id} - Bus Voltage: {bus_voltage:.3f} [V], Bus Current: {bus_current:.3f} [A]")
+
+
+def handle_iq_setpoint_measured_response(node_id, response):
+    iq_setpoint, iq_measured = struct.unpack('<ff', bytes(response.data))
+    print(f"O-Drive {node_id} - Iq Setpoint: {iq_setpoint:.3f} [A], Iq Measured: {iq_measured:.3f} [A]")
+
+
+if __name__ == "__main__":
+    for node_id in odrive_node_ids:
+        try:
+            # Example: Send RTR for Set_Axis_State
+            send_rtr_message(node_id, 0x07)
+
+            # Example: Send RTR for Get_Encoder_Estimates
+            send_rtr_message(node_id, 0x09)
+
+            # Example: Send RTR for Get_Torques
+            send_rtr_message(node_id, 0x1C)
+
+            # Example: Send RTR for Get_Bus_Voltage_Current
+            send_rtr_message(node_id, 0x17)
+
+            # Example: Send RTR for Get_Iq
+            send_rtr_message(node_id, 0x14)
+
+        except KeyboardInterrupt:
+            print("KeyboardInterrupt: Exiting loop.")
+            break
+
+        time.sleep(0.1)  # Adjust the sleep duration as needed
+"""
