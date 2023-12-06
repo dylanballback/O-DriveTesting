1ed9a915452f55605b0c11de3cb0ff87e173b526
Dylan Ballback <Dylanballback19@gmail.com>
2023-12-06 16:27:41 -0500

    clean data using std and update all graphing funcs

--- IP_torqueReactionTesting/plotData.py
+++ IP_torqueReactionTesting/plotData.py
@@ -2,44 +2,18 @@ from torqueReactionTestDatabase import TorqueReactionTestDatabase
 import matplotlib.pyplot as plt
 import numpy as np
 
-def remove_outliers(data):
-    data = np.array(data)
-    Q1 = np.percentile(data, 25)
-    Q3 = np.percentile(data, 75)
-    IQR = Q3 - Q1
-    lower_bound = Q1 - 1.5 * IQR
-    upper_bound = Q3 + 1.5 * IQR
-    cleaned_data = data[(data >= lower_bound) & (data <= upper_bound)]
-    return cleaned_data.tolist()
-
-def remove_outliers_std(data_pairs, num_std=3):
-    # Separate times and values
-    times, values = zip(*data_pairs)
-    values = np.array(values)
-    
-    mean = np.mean(values)
-    std = np.std(values)
-    
-    # Filter pairs where values are within num_std standard deviations from the mean
-    filtered_pairs = [(t, v) for t, v in zip(times, values) if (mean - num_std * std < v < mean + num_std * std)]
-    return filtered_pairs
 
 def plot_torque_data(db, trial_id):
-    trial_data = db.get_trial_data(trial_id)
+    trial_data = db.get_trial_data(trial_id, cleaned=True)  # Fetch cleaned data
 
     if trial_data:
         # Extracting each column
         times, torque_setpoints, torque_estimates, velocities = zip(*trial_data)
 
-        # Cleaning the data
-        cleaned_torque_setpoints = remove_outliers(torque_setpoints)
-        cleaned_torque_estimates = remove_outliers(torque_estimates)
-
-
         # Plotting
         plt.figure(figsize=(10, 6))
-        plt.plot(times[:len(cleaned_torque_setpoints)], cleaned_torque_setpoints, label='Torque Setpoint')
-        plt.plot(times[:len(cleaned_torque_estimates)], cleaned_torque_estimates, label='Torque Estimate')
+        plt.plot(times, torque_setpoints, label='Torque Setpoint')
+        plt.plot(times, torque_estimates, label='Torque Estimate')
         plt.xlabel('Time (seconds)')
         plt.ylabel('Torque')
         plt.title(f'Torque Setpoint and Estimate for Trial ID {trial_id}')
@@ -51,21 +25,11 @@ def plot_torque_data(db, trial_id):
 
 
 def plot_torque_and_velocity_data(db, trial_id):
-    trial_data = db.get_trial_data(trial_id)
+    trial_data = db.get_trial_data(trial_id, cleaned=True)  # Fetch cleaned data
 
     if trial_data:
         # Extracting each column
         times, torque_setpoints, torque_estimates, velocities = zip(*trial_data)
-
-        # Cleaning the data
-        cleaned_torque_setpoints = remove_outliers(torque_setpoints)
-        cleaned_torque_estimates = remove_outliers(torque_estimates)
-        cleaned_velocities = remove_outliers(velocities)
-
-
-        # Ensure all cleaned data lists are of the same length
-        min_length = min(len(cleaned_torque_setpoints), len(cleaned_torque_estimates), len(cleaned_velocities))
-        times = times[:min_length]
 
         # Create the main figure and first y-axis
         fig, ax1 = plt.subplots(figsize=(10, 6))
@@ -74,8 +38,8 @@ def plot_torque_and_velocity_data(db, trial_id):
         color = 'tab:blue'
         ax1.set_xlabel('Time (seconds)')
         ax1.set_ylabel('Torque', color=color)
-        ax1.plot(times, cleaned_torque_setpoints[:min_length], label='Torque Setpoint', color='yellow')
-        ax1.plot(times, cleaned_torque_estimates[:min_length], label='Torque Estimate', color='blue')
+        ax1.plot(times, torque_setpoints, label='Torque Setpoint', color='yellow')
+        ax1.plot(times, torque_estimates, label='Torque Estimate', color='blue')
         ax1.tick_params(axis='y', labelcolor=color)
         ax1.legend(loc='upper left')
 
@@ -83,7 +47,7 @@ def plot_torque_and_velocity_data(db, trial_id):
         ax2 = ax1.twinx()  
         color = 'tab:red'
         ax2.set_ylabel('Velocity', color=color)
-        ax2.plot(times, cleaned_velocities[:min_length], label='Velocity', color='red')
+        ax2.plot(times, velocities, label='Velocity', color='red')
         ax2.tick_params(axis='y', labelcolor=color)
         ax2.legend(loc='upper right')
 
@@ -94,11 +58,45 @@ def plot_torque_and_velocity_data(db, trial_id):
         print(f"No data available for Trial ID {trial_id}")
 
 
+trial_id = int(input("Enter trial ID to plot:"))
 
 
 db = TorqueReactionTestDatabase("torqueReactionTestDatabase.db")
 
-trial_id = int(input("Enter trial ID to plot:"))
+#Remove outliers and create new clean data table.
+#db.remove_outliers_and_create_clean_table(trial_id)
+
 
 plot_torque_data(db, trial_id)  
 plot_torque_and_velocity_data(db, trial_id)  
+
+
+def plot_trial_data(db, trial_id, data_type):
+    """
+    Plots specified data type against time for a given trial ID.
+
+    Parameters:
+    db (TorqueReactionTestDatabase): Database object.
+    trial_id (int): The ID of the trial to plot.
+    data_type (str): The type of data to plot (e.g., 'pos', 'vel', etc.).
+    """
+    trial_data = db.get_data_for_plotting(trial_id, data_type, cleaned=True)  # Fetch cleaned data
+
+    # Unpack the data
+    times, values = zip(*trial_data)
+
+    # Plotting
+    plt.figure(figsize=(10, 6))
+    plt.plot(times, values, label=f'{data_type.title()} over Time')
+    plt.xlabel('Time (seconds)')
+    plt.ylabel(f'{data_type.title()}')
+    plt.title(f'{data_type.title()} over Time for Trial ID {trial_id}')
+    plt.legend()
+    plt.show()
+
+
+plot_trial_data(db, trial_id, data_type='vel')
+
+plot_trial_data(db, trial_id, data_type='torque_estimate')
+
+plot_trial_data(db, trial_id, data_type='bus_current')
--- IP_torqueReactionTesting/torqueReactionTestDatabase.py
+++ IP_torqueReactionTesting/torqueReactionTestDatabase.py
@@ -1,5 +1,7 @@
 from sqlite3 import Error
 import sqlite3
+import numpy as np
+
 
 class TorqueReactionTestDatabase:
 
@@ -104,11 +106,182 @@ class TorqueReactionTestDatabase:
         cur.execute('''DELETE FROM trials WHERE trial_id=?''', (trial_id,))
         self.conn.commit()
 
-    def get_trial_data(self, trial_id):
+    def get_trial_data(self, trial_id, cleaned=False):
         """
-        Fetch all data for a given trial ID.
+        Fetch all data for a given trial ID from either the original or cleaned data table.
         """
-        sql = ''' SELECT time, torque_setpoint, torque_estimate, vel FROM data WHERE trial_id=? '''
+        table_name = "cleaned_data" if cleaned else "data"
+        sql = f''' SELECT time, torque_setpoint, torque_estimate, vel FROM {table_name} WHERE trial_id=? '''
         cur = self.conn.cursor()
         cur.execute(sql, (trial_id,))
         return cur.fetchall()
+
+    def get_data_for_plotting(self, trial_id, data_type, cleaned=False):
+        """
+        Fetch a specific type of data and corresponding time values for a given trial ID
+        from either the original or cleaned data table.
+        """
+        table_name = "cleaned_data" if cleaned else "data"
+        sql = f"SELECT time, {data_type} FROM {table_name} WHERE trial_id=?"
+        cur = self.conn.cursor()
+        cur.execute(sql, (trial_id,))
+        return cur.fetchall()
+
+#--------------------------- Cleaning Data Functions --------------------------------------------------------
+
+    def remove_outliers_std(self, data, num_std=1.6):
+        """
+        Removes outliers from a dataset based on standard deviation.
+
+        Parameters:
+        data (list): The dataset to be cleaned.
+        num_std (int): Number of standard deviations to use as the threshold. Default is 2.
+
+        Returns:
+        list: The cleaned dataset with outliers removed.
+        """
+        mean = np.mean(data)
+        std_dev = np.std(data)
+        lower_bound = mean - num_std * std_dev
+        upper_bound = mean + num_std * std_dev
+
+        cleaned_data = [x for x in data if lower_bound <= x <= upper_bound]
+        return cleaned_data
+
+
+    def clean_trial_data(self, trial_id):
+        # Fetch data for the trial
+        cur = self.conn.cursor()
+        cur.execute("SELECT * FROM data WHERE trial_id=?", (trial_id,))
+        rows = cur.fetchall()
+
+        # Assuming your data columns are: time, pos, vel, torque_setpoint, torque_estimate, bus_voltage, bus_current, iq_setpoint, iq_measured
+        # Adjust the indices based on your actual table structure
+        columns = {'pos': 3, 'vel': 4, 'torque_setpoint': 5, 'torque_estimate': 6, 'bus_voltage': 7, 'bus_current': 8, 'iq_setpoint': 9, 'iq_measured': 10}
+        cleaned_data = []
+
+        # Clean each column separately
+        for col_name, col_idx in columns.items():
+            col_data = [row[col_idx] for row in rows]
+            cleaned_col_data = remove_outliers(col_data)
+            for i, row in enumerate(rows):
+                if col_data[i] in cleaned_col_data:
+                    cleaned_data.append(row)
+
+        # Create a new table for cleaned data
+        cur.execute(
+            '''CREATE TABLE IF NOT EXISTS cleaned_data (
+               id INTEGER PRIMARY KEY AUTOINCREMENT,
+               trial_id INTEGER,
+               time REAL,
+               pos REAL,
+               vel REAL,
+               torque_setpoint REAL,
+               torque_estimate REAL,
+               bus_voltage REAL,
+               bus_current REAL,
+               iq_setpoint REAL,
+               iq_measured REAL,
+               FOREIGN KEY (trial_id) REFERENCES trials (trial_id)
+            );'''
+        )
+
+        # Insert cleaned data into the new table
+        cur.executemany("INSERT INTO cleaned_data (trial_id, time, pos, vel, torque_setpoint, torque_estimate, bus_voltage, bus_current, iq_setpoint, iq_measured) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", cleaned_data)
+        self.conn.commit()
+
+    
+    def remove_outliers_and_create_clean_table(self, trial_id):
+        # Fetch data for the trial
+        cur = self.conn.cursor()
+        cur.execute("SELECT * FROM data WHERE trial_id=?", (trial_id,))
+        rows = cur.fetchall()
+
+        # Assuming your data columns are: time, pos, vel, torque_setpoint, torque_estimate, bus_voltage, bus_current, iq_setpoint, iq_measured
+        columns = {'pos': 3, 'vel': 4, 'torque_setpoint': 5, 'torque_estimate': 6, 'bus_voltage': 7, 'bus_current': 8, 'iq_setpoint': 9, 'iq_measured': 10}
+
+        # Clean each column separately using the standard deviation method
+        cleaned_data = []
+        for i, row in enumerate(rows):
+            is_outlier = False
+            for col_name, col_idx in columns.items():
+                col_data = [r[col_idx] for r in rows]
+                cleaned_col_data = self.remove_outliers_std(col_data)
+
+                if row[col_idx] not in cleaned_col_data:
+                    is_outlier = True
+                    break
+
+            if not is_outlier:
+                cleaned_data.append(row)
+
+        # Create a new table for cleaned data
+        cur.execute(
+            '''CREATE TABLE IF NOT EXISTS cleaned_data (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            trial_id INTEGER,
+            time REAL,
+            pos REAL,
+            vel REAL,
+            torque_setpoint REAL,
+            torque_estimate REAL,
+            bus_voltage REAL,
+            bus_current REAL,
+            iq_setpoint REAL,
+            iq_measured REAL,
+            FOREIGN KEY (trial_id) REFERENCES trials (trial_id)
+            );'''
+        )
+
+        # Insert cleaned data into the new table
+        cur.executemany(
+            "INSERT INTO cleaned_data (trial_id, time, pos, vel, torque_setpoint, torque_estimate, bus_voltage, bus_current, iq_setpoint, iq_measured) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", 
+            [(row[1], row[2], row[3], row[4], row[5], row[6], row[7], row[8], row[9], row[10]) for row in cleaned_data]
+        )
+        self.conn.commit()
+
+    #no longer using, now using STD to remove outliers
+    def identify_outliers(self, data):
+        """
+        Identifies outliers in a dataset using the IQR method.
+
+        Parameters:
+        data (list): The dataset to analyze.
+
+        Returns:
+        set: A set of indices of the outliers in the data.
+        """
+        data_array = np.array(data)
+        Q1 = np.percentile(data_array, 45)
+        Q3 = np.percentile(data_array, 95)
+        IQR = Q3 - Q1
+        lower_bound = Q1 - 1.5 * IQR
+        upper_bound = Q3 + 1.5 * IQR
+
+        outliers = {i for i, x in enumerate(data_array) if x < lower_bound or x > upper_bound}
+        return outliers
+
+#------------------------ Delete specific table ----------------------------------
+    def delete_table(self, table_name):
+            """
+            Deletes a table from the database.
+
+            Parameters:
+            table_name (str): The name of the table to delete.
+            """
+            try:
+                cur = self.conn.cursor()
+                cur.execute(f"DROP TABLE IF EXISTS {table_name}")
+                self.conn.commit()
+                print(f"Table {table_name} deleted successfully.")
+            except Error as e:
+                print(f"Error deleting table {table_name}: {e}")
+
+# Example usage:
+#db = TorqueReactionTestDatabase("torqueReactionTestDatabase.db")
+#db.delete_table("cleaned_data")
+
+#db = TorqueReactionTestDatabase("torqueReactionTestDatabase.db")
+
+#Remove outliers and create new clean data table.
+#db.remove_outliers_and_create_clean_table(trial_id=29)
