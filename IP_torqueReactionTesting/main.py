51b3a30f7e1f18f0cf35d75afb113e3465d28dd1
Dylan Ballback <Dylanballback19@gmail.com>
2023-12-05 23:32:42 -0500

    a

--- IP_torqueReactionTesting/main.py
+++ IP_torqueReactionTesting/main.py
@@ -13,35 +13,41 @@ def main():
     print(f"Added Trial with ID: {trial_id}")
 
     # Define your torque values here
-    torques = [0, 0.005, 0]
+    torques = [0, 0.05, 0]
     measurement_interval = 0.01  # Time between measurements
     torque_change_delay = 5  # Time to wait before changing torque
 
+    # Capture the start time
+    start_time = time.time()
+
+    # Iterate through each torque setting
     for torque in torques:
         # Set torque
         odrive.set_torque(torque)
         print(f"Torque set to {torque} Nm")
 
-        # Measure and store data for a while after setting torque
-        start_time = time.time()
-        while time.time() - start_time < torque_change_delay:
-            current_time = time.time()
-            data_dict = odrive.get_all_data_rtr()
+        # Wait some time for the system to stabilize
+        time.sleep(5)
 
-            data_tuple = (
-                current_time,
-                data_dict.get('pos', 0),
-                data_dict.get('vel', 0),
-                data_dict.get('torque_setpoint', 0),
-                data_dict.get('torque_estimate', 0),
-                data_dict.get('bus_voltage', 0),
-                data_dict.get('bus_current', 0),
-                data_dict.get('iq_setpoint', 0),
-                data_dict.get('iq_measured', 0)
-            )
+        # Calculate elapsed time since start
+        elapsed_time = time.time() - start_time
 
-            torque_reaction_test_database.add_data(trial_id, *data_tuple)
-            time.sleep(measurement_interval)
+        # Measure and store data
+        data_dict = odrive.get_all_data_rtr()
+
+        data_tuple = (
+            elapsed_time,
+            data_dict.get('pos', 0),
+            data_dict.get('vel', 0),
+            data_dict.get('torque_setpoint', 0),
+            data_dict.get('torque_estimate', 0),
+            data_dict.get('bus_voltage', 0),
+            data_dict.get('bus_current', 0),
+            data_dict.get('iq_setpoint', 0),
+            data_dict.get('iq_measured', 0)
+        )
+
+        torque_reaction_test_database.add_data(trial_id, *data_tuple)
 
     print("All torque values have been processed.")
 
