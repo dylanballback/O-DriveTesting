9dcf599a8e5dc88f96696c67a89a6e120de84e83
Dylan Ballback <Dylanballback19@gmail.com>
2023-12-08 23:52:24 -0500

    aa

--- jacob/main.py
+++ jacob/main.py
@@ -2,13 +2,21 @@ import time
 from ODriveCAN import ODriveCAN
 
 
-# Initialize ODriveCAN and database
-odrive = ODriveCAN(0)
-odrive.initCanBus()
+# Initialize ODriveCAN for 1st Motor 
+odrive1 = ODriveCAN(0)
+odrive1.initCanBus()
 
+# Initialize ODriveCAN for 2nd Motor 
+odrive2 = ODriveCAN(1)
+odrive2.initCanBus()
+
+def get_motor2_velocity()
 
 try:
-    odrive.set_velocity(3)
+    while True:
+        odrive.set_velocity(3)
+        pos, vel = odrive.get_encoder_estimate_rtr()
+        print(f"ODrive 1 velocity = {vel} turn/s")
 except KeyboardInterrupt:
     odrive.set_velocity(0)
     print("Program interrupted by user. Exiting.")
