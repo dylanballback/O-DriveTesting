f544c1d8f2d6bd51eeaaaaa9c12681ef0409f1ae
Dylan Ballback <Dylanballback19@gmail.com>
2024-02-07 18:18:31 -0500

    a

--- IP_torqueReactionTesting/testinODriveCAN.py
+++ IP_torqueReactionTesting/testinODriveCAN.py
@@ -17,7 +17,7 @@ def main():
 
     while True:
         odrive.get_encoder_estimate_rtr()
-        #odrive.get_torque_rtr()
+        odrive.get_torque_rtr()
         #odrive.get_bus_voltage_current_rtr()
         #odrive.get_iq_setpoint_measured_rtr()
         #odrive.get_powers_rtr()
