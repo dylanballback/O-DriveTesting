4dfb97a48794abe1f06f21f3db4c118a184323de
Dylan Ballback <Dylanballback19@gmail.com>
2024-02-09 18:13:49 -0500

    update async_recv

--- async_odriveCAN_testing/async_v1.py
+++ async_odriveCAN_testing/async_v1.py
@@ -42,9 +42,7 @@ class ODriveCAN:
             >>> msg = await odrive_can.async_recv(timeout=1.0)
             >>> print(msg)
         """
-        loop = asyncio.get_running_loop()
-        msg = await loop.run_in_executor(None, lambda: self.canBus.recv(timeout))
-        return msg
+        return self.canBus.recv(timeout)
 
 
 #-------------------------------------- O-Drive CAN SETUP START-------------------------------------------------
@@ -492,7 +490,7 @@ async def main():
     
     # Start data collection in the background
     # This task will keep running and collect data into odrive_can.collected_data
-    data_collection_task = asyncio.create_task(odrive_can.data_collection_loop(0.001, next_trial_id))
+    data_collection_task = asyncio.create_task(odrive_can.data_collection_loop(0.1, next_trial_id))
     
     # Sequentially change torque and wait
     # Note: Replace odrive_can.set_torque() with the correct method to set torque if different
