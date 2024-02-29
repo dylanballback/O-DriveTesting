d731d89935949a542968ff85f9cea6ed13ad1f20
Dylan Ballback <Dylanballback19@gmail.com>
2024-02-29 16:35:40 -0500

    update k

--- 1DOF_PID/drakunov_controls/main.py
+++ 1DOF_PID/drakunov_controls/main.py
@@ -167,7 +167,7 @@ async def main():
 
     #Controller Consts
     J_zz = 0.001666667
-    K = 1
+    K = 5
     controller_trial_notes = "Here we can take notes on our controller"
 
     controller_param_data = (next_trial_id, J_zz, K, "Some notes about the controller trial")
