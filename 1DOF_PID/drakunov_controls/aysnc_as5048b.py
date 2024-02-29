baf051b2da107324adf1d57b993e58db4fafd9cf
Dylan Ballback <Dylanballback19@gmail.com>
2024-02-29 16:13:41 -0500

    add controls and reformat repo

---
+++ 1DOF_PID/drakunov_controls/aysnc_as5048b.py
@@ -0,0 +1,247 @@
+import asyncio
+from dataclasses import dataclass, field
+from datetime import datetime, timedelta
+import pyodrivecan
+import time
+import math
+
+from smbus import SMBus
+
+
+@dataclass
+class Encoder_as5048b:
+    """
+    A class to represent an AS5048B magnetic rotary encoder.
+
+    Attributes:
+    - bus (SMBus): The SMBus object for I2C communication.
+    - address (int): The I2C address of the AS5048B encoder.
+    - angle_reg (int): The register address to read the angle from.
+    - angle (float): The latest read angle value after offset adjustment.
+    - offset (float): The calibrated offset value for the angle.
+    - running (bool): Flag to control the asynchronous angle reading loop.
+    - printing (bool): Flag for controlling the printing of the encoder angle.
+    - previous_angle (float): Stores the previous angle for velocity calculation.
+    - previous_time (float): Stores the time when the previous angle was read.
+    - angular_velocity (float): Angular velocity in radians/second.
+    - database (pyodrivecan.OdriveDatabase): Database object for storing encoder data.
+    - table_name (str): Name of the table for storing encoder data.
+    - start_time (float): Captures the start time when the object is initialized.
+    """
+    bus: SMBus = field(default_factory=lambda: SMBus(1))
+    address: int = 0x40  # AS5048B default address
+    angle_reg: int = 0xFE  # AS5048B Register
+    angle: float = 0.0  # Initialized angle
+    offset: float = 0.0  # Initial offset
+    running: bool = True  # Control flag for running the loop
+    printing: bool = False  # Control flag for printing encoder angle
+    previous_angle: float = 0.0  # To store the previous angle
+    previous_time: float = time.time()  # To store the time when the previous angle was read
+    angular_velocity: float = 0.0  # Angular velocity in radians/second
+    database: pyodrivecan.OdriveDatabase = field(default_factory=lambda: pyodrivecan.OdriveDatabase('odrive_data.db'))
+    table_name: str = 'encoderData'  # Name of the table for storing encoder data
+    start_time: float = time.time()  # Capture the start time when the object is initialized
+
+
+
+    def read_angle(self):
+        """
+        Reads the current angle from the AS5048B magnetic rotary encoder.
+
+        This method communicates with the encoder via I2C to read the raw angle data from its designated register.
+        The raw data is then converted to an angle in degrees, considering the specific gear ratio used in the application.
+        Finally, the calibrated offset is subtracted to adjust the angle based on the initial calibration.
+
+        Returns:
+            float: The current angle in degrees, adjusted for the calibrated offset.
+        """
+        data = self.bus.read_i2c_block_data(self.address, self.angle_reg, 2)
+        angle = data[0] * 256 + data[1]
+        angle *= 90 / 16383 # Convert raw data to angle in degrees 
+        return angle - self.offset # Adjust by offset
+
+
+    def calibrate(self):
+        """
+        Calibrates the encoder by setting the current angle as the zero offset.
+
+        This method reads the current angle from the encoder and sets this value as the offset.
+        It is assumed that the encoder is in the desired zero position at the time of calibration.
+        A 5-second delay is provided after setting the offset to allow for manual adjustment of the encoder's position.
+        """
+        self.offset = self.read_angle()
+        print("Calibration Complete move to top, you have 5 seconds.")
+        time.sleep(5) #Wait 5 seconds after calibrate to move to top
+        print("5 Seconds Over, Going to run controller.")
+
+
+    async def calculate_angular_velocity(self):
+        """
+        Asynchronously calculates and updates the angular velocity in radians per second.
+
+        This method computes the angular velocity by calculating the difference between the current and previous angle readings,
+        converting this difference from degrees to radians, and then dividing by the time difference between these readings.
+        The calculated angular velocity is stored and updated at each call.
+
+        Note: This method should be called repeatedly in a loop to continuously update the angular velocity.
+        """
+        current_angle = self.read_angle()
+        current_time = time.time()
+        angle_difference = current_angle - self.previous_angle
+        time_difference = current_time - self.previous_time
+
+        # Convert angle difference from degrees to radians
+        angle_difference_radians = math.radians(angle_difference)
+
+        # Calculate angular velocity (radians per second)
+        if time_difference != 0:  # Avoid division by zero
+            self.angular_velocity = angle_difference_radians / time_difference
+
+        # Update previous angle and time for the next calculation
+        self.previous_angle = current_angle
+        self.previous_time = current_time
+
+
+
+    async def listen_to_angle(self):
+        """
+        An asynchronous loop that continuously reads the encoder's angle and calculates the angular velocity.
+
+        This loop runs indefinitely (until the `running` flag is set to False), reading the current angle from the encoder
+        and calculating the angular velocity at each iteration. It uses a non-blocking sleep to yield control, allowing other
+        tasks to run concurrently.
+        """
+        while self.running:
+            await asyncio.sleep(0) # Non-blocking sleep to yield control
+            self.angle = self.read_angle() # Update the current angle
+            await self.calculate_angular_velocity()  # Calculate angular velocity
+
+
+
+    #Function to setup Custom Encoder Table in the Database 
+    def encoder_table_init(self):
+        """
+        Initializes a custom table in the database for storing encoder data.
+
+        This method defines the structure of the table with columns for the angle and timestamp.
+        It then creates this table in the database, ready for storing encoder readings.
+        """
+
+
+        #Define the table column name and SQL data type
+        table_columns_type = [
+            ("time", "REAL"),
+            ("angle", "REAL"),
+            ("velocity", "REAL")
+        ]
+
+        #Create encoderData table
+        self.database.create_user_defined_table(self.table_name, table_columns_type)
+
+
+    async def save_angle_loop(self): 
+        """
+        An asynchronous loop that saves the latest encoder angle, angular velocity, and timestamp to the database.
+
+        This loop runs indefinitely until the `running` flag is set to False. It captures the latest encoder angle and
+        angular velocity, calculates the elapsed time since the start of the program, and inserts these values along
+        with a trial ID into a predefined table in the database. The database table is structured to store the timestamp,
+        angle in degrees, and angular velocity in radians per second for each recorded entry.
+
+        This method allows for continuous logging of encoder data for analysis or real-time monitoring purposes, 
+        providing valuable insights into the encoder's performance and the system's dynamics.
+
+        Attributes captured and stored:
+        - Trial ID: A unique identifier for the set of measurements, facilitating the grouping of data points.
+        - Time: The elapsed time in seconds from the start of data collection.
+        - Angle: The current angle of the encoder in degrees, adjusted for any offset.
+        - Angular Velocity: The current angular velocity of the encoder in radians per second, calculated from successive angle measurements.
+        """
+        next_trial_id = self.database.get_next_trial_id()
+        
+        while self.running:
+            await asyncio.sleep(0) # Non-blocking sleep to yield control
+            #Define the columns of the encoderData table
+            columns = ["trial_id", "time", "angle", "velocity"]
+
+            #Get latest angle to add to database
+            current_angle = self.angle
+            #print(current_angle)
+
+            #Get latest angular velocity to add to database
+            current_angular_velocity = self.angular_velocity
+            #print(current_angular_velocity)
+            
+            # Calculate elapsed time since the start of the program
+            current_time = time.time() - self.start_time
+
+            values = [next_trial_id, current_time, current_angle, current_angular_velocity]
+            #print(values)
+
+            #Enter latest data in values into the database encoder table.
+            self.database.insert_into_user_defined_table(self.table_name, columns, values)
+
+
+
+    async def loop(self, *others):
+        """
+        Runs the `listen_to_angle` method alongside other asynchronous tasks.
+
+        This method uses `asyncio.gather` to concurrently execute the `listen_to_angle` method, the `save_angle_loop`,
+        and any additional provided asynchronous tasks.
+
+        Args:
+            *others: Additional asyncio tasks to run concurrently with the angle listening and saving loops.
+        """
+        await asyncio.gather(
+            self.listen_to_angle(),
+            self.save_angle_loop(),
+            *others,
+        )
+        
+
+    def run(self, *others):
+        """
+        A convenience method to start the asynchronous loop, wrapping the `loop` method call with `asyncio.run`.
+
+        This method is the entry point for running the encoder's asynchronous functionality, including continuous
+        angle reading, angular velocity calculation, and data logging.
+
+        Args:
+            *others: Additional asyncio tasks to run concurrently with the main loop.
+        """
+        asyncio.run(self.loop(*others))
+
+    
+async def controller(encoder):
+    """
+    A simple control loop that prints the encoder's angle for a set duration or indefinitely.
+
+    This function represents a basic example of a control task that utilizes the encoder's angle reading.
+    It continually prints the encoder's current angle at a high frequency, demonstrating real-time monitoring.
+
+    Args:
+        encoder (Encoder_as5048b): An instance of the `Encoder_as5048b` class to monitor.
+    """
+    #Create the Encoder Database Table with the Initalization Function
+    
+    #stop_at = datetime.now() + timedelta(seconds=15)
+    
+    #while datetime.now() < stop_at:
+    while True:
+        await asyncio.sleep(0.001) # Reduce sleep to get angles faster
+        print("Encoder angle: ", encoder.angle)
+        print("Encoder angular velocity: ", encoder.angular_velocity)
+        
+    
+    encoder.running = False # Stop the encoder's listening loop
+
+
+"""
+Example
+
+if __name__ == "__main__":
+    encoder = Encoder_as5048b()
+    encoder.calibrate()
+    encoder.run(controller(encoder))
+"""
---
+++ 1DOF_PID/drakunov_controls/main.py
@@ -0,0 +1,196 @@
+import pyodrivecan
+import asyncio
+from datetime import datetime, timedelta
+import aysnc_as5048b
+
+
+#------------------------ Controller Parameters from each Trial --------------------------------------------
+
+#Function to setup Custom Controller parameters Table in the  Database
+def controller_param_table_init(database,  controller_param_table_name):
+
+    #Define the table column name and SQL data type
+    table_columns_type = [
+        ("J_zz", "REAL"),
+        ("K", "REAL"),
+        ("notes", "TEXT")
+    ]
+
+    #Create table
+    database.create_user_defined_table(controller_param_table_name, table_columns_type)
+
+
+def upload_controller_parameters(database, controller_param_table_name, controller_params_data):
+     """
+     This will be a function that will take the latest encoder value and upload it to the database.
+     """
+     #Define the columns of the PID Parameters table
+     columns = ["trial_id", "J_zz", "K", "notes"]
+
+     values = controller_params_data
+
+     database.insert_into_user_defined_table(controller_param_table_name, columns, values)
+
+
+#------------------------ Controller Data from each cycle --------------------------------------------
+
+def controller_data_table_init(database, controller_data_table_name):
+        
+    table_columns_type = [
+        ("current_time", "REAL"),
+        ("omega_z", "REAL"),
+        ("u_Z", "REAL"),
+    ]
+
+    #Create table
+    database.create_user_defined_table(controller_data_table_name, table_columns_type)
+
+
+async def upload_controller_data(database, controller_data_table_name, controller_data):
+    """
+    Asynchronously uploads the latest encoder value and related data to the database.
+
+    Parameters:
+    - database: The database object to perform operations on.
+    - controller_data_table_name: The name of the table where data will be inserted.
+    - controller_data: The data to upload to the database.
+    """
+    #Define the columns of the PID Parameters table
+    columns = ["trial_id", "current_time", "omega_z", "u_Z"]
+
+    values = controller_data
+
+    #database.insert_into_user_defined_table(controller_data_table_name, columns, values)
+    # Use run_in_executor to run the synchronous database insert operation in a background thread
+    loop = asyncio.get_running_loop()
+    await loop.run_in_executor(None, database.insert_into_user_defined_table, controller_data_table_name, columns, values)
+
+
+
+
+
+#Functions to clamp a variables upper and lower limit.        
+def clamp(x, lower, upper):
+    return lower if x < lower else upper if x > upper else x
+
+
+def control_law_single_axis(J_zz, K, omega_z):
+    """
+    Calculate the control input (torque) for rotation about the z-axis.
+
+    Parameters:
+    - J_zz: Moment of inertia about the z-axis.
+    - K: Control gain for the z-axis.
+    - omega_z: Angular velocity about the z-axis.
+
+    Returns:
+    - u_z: Control input (torque) about the z-axis in Nm.
+    """
+    u_z = -J_zz * K * omega_z
+    return u_z
+
+
+#Example of how you can create a controller to get data from the O-Drives and then send motor comands based on that data.
+async def controller(odrive1, encoder, database, controller_data_table_name, next_trial_id, J_zz, K):
+        odrive1.clear_errors(identify=False)
+        await asyncio.sleep(0.2)
+        odrive1.setAxisState("closed_loop_control")
+        await asyncio.sleep(0.2)
+        odrive1.set_torque(0)
+
+
+        await asyncio.sleep(2)
+        #Run for set time delay example runs for 15 seconds.
+        stop_at = datetime.now() + timedelta(seconds=100000)
+        while datetime.now() < stop_at:
+            await asyncio.sleep(0) #Need this for async to work.
+            
+            #Get the current angle of the encoder
+            #current_angle = encoder.angle
+
+            # Get the current angluar velocity of the encoder
+            current_angular_velocity = encoder.angular_velocity
+
+
+            # Check if the pendulum has fallen by comparing the current angle with the thresholds
+            #if current_angle < angle_threshold_min or current_angle > angle_threshold_max:
+                #print("Pendulum has fallen. Initiating emergency stop.")
+                #odrive1.estop()
+                #break  # Exit the loop to stop further execution
+
+            #Input the current encoder angular velocity into the Controller and get its torque output
+            controller_torque_output = control_law_single_axis(J_zz, K, current_angular_velocity)
+            print(f"PID Output: {controller_torque_output}, Current Angular Velocity: {current_angular_velocity}")
+
+            #Send pid_output to control motor Torque
+            odrive1.set_torque(controller_torque_output)
+
+            data = [next_trial_id, encoder.previous_time, current_angular_velocity, controller_torque_output]
+            #Add to database
+            upload_controller_data( database, controller_data_table_name, data)
+          
+
+            
+        #await asyncio.sleep(15) #no longer need this the timedelta =15 runs the program for 15 seconds.
+        odrive1.running = False
+        odrive1.estop()
+
+
+
+
+
+# Run multiple busses.
+async def main():
+    #Set up Node_ID 0
+    odrive1 = pyodrivecan.ODriveCAN(0)
+    odrive1.initCanBus()
+    
+    print(odrive1.database)
+    #This sets up the database path the same as odrive1 object.
+    database = pyodrivecan.OdriveDatabase('odrive_data.db')
+
+    #This gets the next trial id from the database
+    next_trial_id = database.get_next_trial_id()
+    print(f"Using trial_id: {next_trial_id}")
+
+    #Set up Encoder 
+    encoder = aysnc_as5048b.Encoder_as5048b()
+    encoder.calibrate()
+    encoder.encoder_table_init()
+
+    #Controller Parameters Table Name
+    controller_param_table_name = 'controllerParameters'
+
+    #Create the PID Parameters Database Table with the Initalization Function
+    upload_controller_parameters(database, controller_param_table_name)
+
+    #Controller Consts
+    J_zz = 0.001666667
+    K = 1
+    controller_trial_notes = "Here we can take notes on our controller"
+
+    controller_param_data = (next_trial_id, J_zz, K, controller_trial_notes)
+    #Upload PID parameters and notes to database
+    upload_controller_parameters(database, controller_param_table_name, controller_param_data)
+
+    controller_data_table_name = 'controllerData'
+
+    #Set up Controller database table
+    controller_data_table_init(database, controller_data_table_name)
+
+
+    try:
+        #add each odrive to the async loop so they will run.
+        await asyncio.gather(
+            odrive1.loop(),
+            controller(odrive1, encoder, database, controller_data_table_name, next_trial_id, J_zz, K), 
+            encoder.loop(), #This runs the external encoder code
+        )
+    except KeyboardInterrupt:
+         odrive1.estop()
+
+
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
