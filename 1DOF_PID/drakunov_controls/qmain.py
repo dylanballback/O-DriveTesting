c2e1163140f68397e9a698f8ec76f00b76abb7e8
Dylan Ballback <Dylanballback19@gmail.com>
2024-03-01 13:20:35 -0500

    update

--- 1DOF_PID/drakunov_controls/qmain.py
+++ 1DOF_PID/drakunov_controls/qmain.py
@@ -198,6 +198,7 @@ async def controller(odrive1, encoder, database, controller_data_table_name, nex
 
         last_time = time.time()  # Capture the current time
         last_angle = 0 
+        angle_error_prev = 0
         #q_desired = angle_to_quaternion(desired_attitude_deg)
         #q_error_prev = np.array([1, 0, 0, 0])  # Assume starting with no error
         
