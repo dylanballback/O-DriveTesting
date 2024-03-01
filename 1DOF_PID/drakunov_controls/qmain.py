4084e658f1b29d009f671ecaff5f31f2e8ae8417
Dylan Ballback <Dylanballback19@gmail.com>
2024-03-01 14:07:20 -0500

    update gains

--- 1DOF_PID/drakunov_controls/qmain.py
+++ 1DOF_PID/drakunov_controls/qmain.py
@@ -313,8 +313,8 @@ async def main():
     controller_trial_notes = "Here we can take notes on our controller"
 
     #For Quaternion Control Desired Angular Velocity PD Controller
-    Kp = 0.1
-    Kd = 0.06
+    Kp = 0.05
+    Kd = 0.001
     desired_attitude_deg = 30 #Degrees
 
     controller_param_data = (next_trial_id, J_zz, K, "Some notes about the controller trial")
