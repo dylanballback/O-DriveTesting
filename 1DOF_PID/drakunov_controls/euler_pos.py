297b1ec08c17aa7358e464733a569dc44c2b2987
Dylan Ballback <Dylanballback19@gmail.com>
2024-03-03 15:48:51 -0500

    kp

--- 1DOF_PID/drakunov_controls/euler_pos.py
+++ 1DOF_PID/drakunov_controls/euler_pos.py
@@ -258,7 +258,7 @@ async def main():
     K = 2
 
     #For Control Desired Angular Velocity PD Controller
-    Kp = 0.025
+    Kp = 0.028
     Kd = 0.0000001
     desired_attitude_deg = 30 #Degrees
 
